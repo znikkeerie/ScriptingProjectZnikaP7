d3d96670df42e5e82c8247c4adb7d4742c19c863
znikkeerie <keiraznika@gmail.com>
2022-11-03 15:24:26 -0700

    Adding Scripts
    Added Two scripts, working on the other two(has to be next period)

---
+++ ScriptingProjectZnikaP7/Assets/Scripts/ForEachLoops.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] strings = new string[3];
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
