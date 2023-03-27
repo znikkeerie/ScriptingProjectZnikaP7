8ee89bb675869f8649b3684a12cb5468bfea9065
znikkeerie <keiraznika@gmail.com>
2023-03-27 14:29:53 -0700

    cREATED NEW SCENE CLASSES AND SCRIPTS
    GOT DONE WITH SCRIPTS, NOW I NEED TO ATTACXH THEM

---
+++ ScriptingProjectZnikaP7/Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+
+        public Stuff(int bul, float fu)
+        {
+            bullets = bul;
+            fuel = fu;
+        }
+
+        public Stuff ()
+        {
+            bullets = 1;
+            grenades = 1;
+            rockets = 1;
+        }
+    }
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50,  1.5f);
+    
+    void Start ()
+    {
+    Debug.Log(myStuff.bullets);
+    }
+}
