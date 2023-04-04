7b232c0fdf2b594976fb854f194c2acd48b7fbb7
znikkeerie <keiraznika@gmail.com>
2023-04-04 15:20:18 -0700

    Arrays
    Made an arrays script, 3 game objects with names, one empty, added the script to the empty, no errors, it is fine and works. Progress is made just fine.

---
+++ ScriptingProjectZnikaP7/Assets/Scripts/Arrays.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public GameObject[] players;
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for(int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number " + i + " is named " + players[i].name);
+        }
+    }
+}
